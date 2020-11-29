ef87662721800e0e33f6d74739ddf010ae4b0508
Rockolino99 <rickymuro9@gmail.com>
2020-11-28 23:09:09 -0600

    New files
    Copiar archivos

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda | Cutsie Girl</title>
+    <!--Links-->
+
+    <!--Fin Links-->
+</head>
+<body>
+    <!--Header-->
+    
+    <!--Fin Header-->
+    <?php
+        if(!isset($mod))
+            $mod = 'main';
+        switch ($mod) {
+            case 'main': //Página Principal
+                include "views/main.php";
+                break;
+            case 'store':
+                include "";
+            break;
+        } 
+    ?>
+</body>
+</html>
---
+++ views/main.php
@@ -0,0 +1,5 @@
+<?php
+echo "Hola mundo<br>";
+echo "xD";
+
+?>
