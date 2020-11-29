17683fa284432e4808f2590ed8ffab585c6e7ea3
lalo0099 <lalopenaesparza@gmail.com>
2020-11-29 00:09:57 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda | Cutsie Girl</title>
     <!--Links-->
-    <link rel="stylesheet" href="css/estilos2.css">
-    <link rel="stylesheet" href="css/estilos.css">
+    
     <!--Fin Links-->
 </head>
 <body>
